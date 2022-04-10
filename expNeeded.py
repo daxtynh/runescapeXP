5a13a28625b6090c9b7f312ce7a2903fe3ed6661
daxtynh <97125660+daxtynh@users.noreply.github.com>
2022-04-09 22:46:17 -0500

    Update expNeeded.py
    Added checking if level is less than 99

--- expNeeded.py
+++ expNeeded.py
@@ -2,8 +2,11 @@ import math
 
 def expNeeded(level):
     total = 0
-    for i in range(1, level):
-        total += math.floor(i + 300 * pow(2, i / 7.0))
+    if(level <= 99):
+        for i in range(1, level):
+            total += math.floor(i + 300 * pow(2, i / 7.0))
+    else:
+        return "Level greater than 99, this isn't RS3"
     return math.floor(total / 4)
 
-print(expNeeded(99))
+print(expNeeded(25))
